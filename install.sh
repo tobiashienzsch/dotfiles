8179aa156f23f678291210b2956ac9b11a45d392
Tobias Hienzsch <post@tobias-hienzsch.de>
2018-12-29 00:09:16 +0100

    Add profile config

--- install.sh
+++ install.sh
@@ -3,5 +3,9 @@
 # Set the current working directory.
 cwd=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
+# NEOVIM
 mkdir -p $HOME/.config/nvim
 ln -sf $cwd/neovim/* $HOME/.config/nvim
+
+# ENV VARIABLES
+ln -sf $cwd/tobanterc.sh $HOME/
---
+++ tobanterc.sh
@@ -0,0 +1,21 @@
+# User configuration
+export MANPATH="/usr/local/man:$MANPATH"
+export LANG=en_US.UTF-8
+export EDITOR='nvim'
+
+# PATH
+export PATH=$PATH:/usr/local/go/bin
+export PATH=$PATH:$HOME/bin
+
+# C++ / JUCE
+export JUCEPATH=$HOME/Developer/lib/JUCE
+export PATH=$JUCEPATH/extras/Projucer/Builds/LinuxMakefile/build:$PATH
+
+# ANDROID
+export ANDROID_SDK=$HOME/Android/Sdk
+
+# GOLANG
+export GOPATH=$HOME/go
+export PATH=$PATH:$HOME/go/bin
+
+
