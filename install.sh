0d3ceca1ef3fd843bf49f0d164d4386745e9af5b
Tobias Hienzsch <post@tobias-hienzsch.de>
2019-05-22 23:13:02 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # This script installs dotfiles onto a unix like
-# system. It checks if all programs are available,
-# creates the config folders and simlinks the content
-# of this repo to the created locations.
+# system. It creates the config folders and simlinks 
+# the content of this repo to the created locations.
 
-cwd=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+CWD=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
 show_help()
 {
@@ -13,84 +12,63 @@ show_help()
     exit 1
 }
 
-# Checks if the program is installed,
-# for which the config is loaded
-check_if_program_exists()
-{
-    PROGRAM=$1  shift;
-    COMMENTS=$@
-
-    if ! [ -x "$(command -v $PROGRAM)" ]; then
-        echo 'Error: '$PROGRAM' is not installed.' >&2
-        exit 1
-    fi
-}
-
 install_zsh()
 {
     # ZSH
-    #check_if_program_exists zsh
     echo "zsh"
-    ln -sf $cwd/zsh/.zshrc $HOME/
+    ln -sf $CWD/zsh/.zshrc $HOME/
 }
 
 install_git()
 {
     # GIT
-    #check_if_program_exists git
     echo "git"
-    ln -sf $cwd/git/.gitconfig $HOME/
+    ln -sf $CWD/git/.gitconfig $HOME/
 }
 
 install_neovim()
 {
     # NEOVIM
-    #check_if_program_exists nvim
     echo "neovim"
     mkdir -p $HOME/.config/nvim
-    ln -sf $cwd/neovim/* $HOME/.config/nvim
+    ln -sf $CWD/neovim/* $HOME/.config/nvim
     curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 }
 
 install_i3()
 {
     # I3
-    check_if_program_exists i3
     echo "i3"
     mkdir -p $HOME/.config/i3
-    ln -sf $cwd/i3/* $HOME/.config/i3
+    ln -sf $CWD/i3/* $HOME/.config/i3
 
     # I3 STATUS
-    check_if_program_exists i3status
     mkdir -p $HOME/.config/i3status
-    ln -sf $cwd/i3status/* $HOME/.config/i3status
+    ln -sf $CWD/i3status/* $HOME/.config/i3status
 }
 
 install_compton()
 {
     # COMPTON
-    check_if_program_exists compton
     echo "compton"
-    ln -sf $cwd/compton/* $HOME/.config
+    ln -sf $CWD/compton/* $HOME/.config
 }
 
 
 install_nitrogen()
 {
     # NITROGEN
-    check_if_program_exists nitrogen
     echo "nitrogen"
     mkdir -p $HOME/.config/nitrogen
-    ln -sf $cwd/nitrogen/* $HOME/.config/nitrogen
+    ln -sf $CWD/nitrogen/* $HOME/.config/nitrogen
 }
 
 install_trizen()
 {
     # TRIZEN
-    check_if_program_exists trizen
     echo "nitrogen"
     mkdir -p $HOME/.config/trizen
-    ln -sf $cwd/trizen/* $HOME/.config/trizen
+    ln -sf $CWD/trizen/* $HOME/.config/trizen
 }
 
 if [ "$1" == "" ]; then
